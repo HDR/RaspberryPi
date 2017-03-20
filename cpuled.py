4f5c93e65824c37fb890d404ce65ecc54e9657d6
MrHDR <refshet@yahoo.no>
2017-03-20 18:17:47 +0100

    1

---
+++ cpuled.py
@@ -0,0 +1,69 @@
+import os
+import time
+from blinkt import set_pixel, show, set_brightness, clear
+
+#Get CPU Temp in Celcius
+def getCPUtemp():
+    res = os.popen('vcgencmd measure_temp').readline()
+    return(res.replace("temp=","").replace("'C\n",""))
+CPU_temp = int(float(getCPUtemp()))
+
+greenPix = 0,255,0
+orangePix = 255,102,0
+redPix = 255,0,0
+
+set_brightness(0.1)
+
+while True:
+ if CPU_temp < 10:
+  set_pixel(0,greenPix)
+  show()
+ if CPU_temp > 10 & CPU_temp < 20:
+  set_pixel(0,0,255,0)
+  set_pixel(1,0,255,0)
+  show()
+ if CPU_temp > 20 & CPU_temp < 30:
+  set_pixel(0,0,255,0)
+  set_pixel(1,0,255,0)
+  set_pixel(2,0,255,0)
+  show()
+ if CPU_temp > 30 & CPU_temp < 40:
+  set_pixel(0,0,255,0)
+  set_pixel(1,0,255,0)
+  set_pixel(2,0,255,0)
+  set_pixel(3,255,102,0)
+  show()
+ if CPU_temp > 40 & CPU_temp < 50:
+  set_pixel(0,0,255,0)
+  set_pixel(1,0,255,0)
+  set_pixel(2,0,255,0)
+  set_pixel(3,255,102,0)
+  set_pixel(4,255,102,0)
+  show()
+ if CPU_temp > 50 & CPU_temp < 60:
+  set_pixel(0,0,255,0)
+  set_pixel(1,0,255,0)
+  set_pixel(2,0,255,0)
+  set_pixel(3,255,102,0)
+  set_pixel(4,255,102,0)
+  set_pixel(5,255,102,0)
+  show()
+ if CPU_temp > 60 & CPU_temp < 70:
+  set_pixel(0,0,255,0)
+  set_pixel(1,0,255,0)
+  set_pixel(2,0,255,0)
+  set_pixel(3,255,102,0)
+  set_pixel(4,255,102,0)
+  set_pixel(5,255,102,0)
+  set_pixel(6,255,0,0)
+  show()
+ if CPU_temp > 70:
+  set_pixel(0,0,255,0)
+  set_pixel(1,0,255,0)
+  set_pixel(2,0,255,0)
+  set_pixel(3,255,102,0)
+  set_pixel(4,255,102,0)
+  set_pixel(5,255,102,0)
+  set_pixel(6,255,0,0)
+  set_pixel(7,255,0,0)
+  show()
