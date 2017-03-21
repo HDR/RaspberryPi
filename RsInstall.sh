793e8e20fc4ac754df14e778bb3c656ea588a067
Martin <MrHDR@users.noreply.github.com>
2017-03-21 18:03:58 +0100

    Update RsInstall.sh

--- RsInstall.sh
+++ RsInstall.sh
@@ -8,6 +8,6 @@ cd $HOME/.jagex/runescape/bin
 wget https://github.com/MrHDR/RaspberryPi/raw/master/jagexappletviewer.jar
 cd $HOME/.jagex/runescape/images
 wget https://raw.githubusercontent.com/MrHDR/RaspberryPi/master/jagexappletviewer.png
-cd $HOME/Desktop
+cd ~/Desktop
 wget https://github.com/MrHDR/RaspberryPi/raw/master/Rs3.desktop
 wget https://github.com/MrHDR/RaspberryPi/raw/master/OSRS.desktop
