570d1feac5275126c99a76a08ae9f3186e7a27fa
Martin <MrHDR@users.noreply.github.com>
2017-03-21 16:09:51 +0100

    Create OSRSLauncher.sh

---
+++ OSRSLauncher.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/java -Xmx256m -Djava.class.path=$HOME/.jagex/runescape/bin/jagexappletviewer.jar -Dcom.jagex.config=$"http://oldschool.runescape.com/jav_config.ws" jagexappletviewer $USERNAME/.jagex/runescape/images
