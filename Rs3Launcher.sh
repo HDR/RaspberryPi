e5c329f727ff42ad949c7102fc8907fe15ad411f
Martin <MrHDR@users.noreply.github.com>
2017-03-21 17:11:23 +0100

    Update and rename Rs3Launcher.sh to Rs3.desktop

--- Rs3Launcher.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Xmx512m -Djava.class.path=/home/pi/.jagex/runescape/bin/jagexappletviewer.jar -Dcom.jagex.config=$"http://www.runescape.com/k=3/l=en/jav_config.ws" jagexappletviewer $USERNAME/.jagex/runescape/images
