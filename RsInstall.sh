df4a5cd85057416678fd94e349faedf64ae65669
MrHDR <refshet@yahoo.no>
2017-03-31 06:19:13 +0200

    Added OSRS Icon.

--- RsInstall.sh
+++ RsInstall.sh
@@ -10,6 +10,7 @@ cd /home/pi/.jagex/runescape/bin
 wget https://github.com/MrHDR/RaspberryPi/raw/master/jagexappletviewer.jar
 cd /home/pi/.jagex/runescape/images
 wget https://raw.githubusercontent.com/MrHDR/RaspberryPi/master/jagexappletviewer.png
+wget https://raw.githubusercontent.com/MrHDR/RaspberryPi/master/jagexappletvieweros.png
 cd /home/pi/Desktop
 wget https://github.com/MrHDR/RaspberryPi/raw/master/Rs3.desktop
 wget https://github.com/MrHDR/RaspberryPi/raw/master/OSRS.desktop
