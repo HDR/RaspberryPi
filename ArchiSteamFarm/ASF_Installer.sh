81ada5bdd597336bb860fca5abce28a61c1e8622
Martin <MrHDR@users.noreply.github.com>
2017-05-05 03:58:12 +0200

    Update ASF_Installer.sh

--- ArchiSteamFarm/ASF_Installer.sh
+++ ArchiSteamFarm/ASF_Installer.sh
@@ -2,3 +2,12 @@ echo "deb http://download.mono-project.com/repo/debian wheezy main # Yes, wheezy
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo apt-get update
 sudo apt-get install ca-certificates-mono mono-complete
+cd /home/pi/
+mkdir ArchiSteamFarm
+cd ArchiSteamFarm
+wget https://github.com/MrHDR/RaspberryPi/raw/master/ArchiSteamFarm/ASF.png
+wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.3.1.3/ASF.zip
+unzip ASF.zip
+cd /home/pi/Desktop
+wget https://raw.githubusercontent.com/MrHDR/RaspberryPi/master/ArchiSteamFarm/ASFC.desktop
+wget https://raw.githubusercontent.com/MrHDR/RaspberryPi/master/ArchiSteamFarm/ASF.desktop
