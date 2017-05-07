9fd9194e067faf7c2d38169609d15bade6a44765
Martin <MrHDR@users.noreply.github.com>
2017-05-07 20:39:26 +0200

    Update ASF_Installer.sh

--- ArchiSteamFarm/ASF_Installer.sh
+++ ArchiSteamFarm/ASF_Installer.sh
@@ -1,7 +1,7 @@
 echo "deb http://download.mono-project.com/repo/debian wheezy main # Yes, wheezy also covers other versions and distributions" >> /etc/apt/sources.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-sudo apt-get update
-sudo apt-get install ca-certificates-mono mono-complete
+sudo apt-get update -y
+sudo apt-get install ca-certificates-mono mono-complete -y
 cd /home/pi/
 mkdir ArchiSteamFarm
 cd ArchiSteamFarm
