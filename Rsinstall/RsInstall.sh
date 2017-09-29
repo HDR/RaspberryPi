d86c47c467c6b8cb243bbb51f562779f99230cb9
Martin <MrHDR@users.noreply.github.com>
2017-09-29 20:27:18 +0200

    Added dialog boxes
    Install script will now look prettier.

--- Rsinstall/RsInstall.sh
+++ Rsinstall/RsInstall.sh
@@ -1,16 +1,27 @@
-sudo apt-get update
-sudo apt-get install oracle-java8-jdk
+#!/bin/bash
+sudo apt-get update | dialog --title "Updating Package database" --infobox "\nPlease wait\n" 5 50
+
+sudo apt-get -y install oracle-java8-jdk | dialog --title "Installing Java 8 JDK" --infobox "\nThis might take a while, please wait\n" 5 50
+
+dialog --title "Creating Folders" --infobox "\nPlease wait\n" 5 50
+
 mkdir /home/pi/.jagex_cache_32
 mkdir /home/pi/.jagex_cache_32/runescape
 mkdir /home/pi/.jagex
 mkdir /home/pi/.jagex/runescape
 mkdir /home/pi/.jagex/runescape/bin
 mkdir /home/pi/.jagex/runescape/images
-cd /home/pi/.jagex/runescape/bin
+
+dialog --title "Downloading & Installing Client" --infobox "\nPlease wait\n" 5 50
+cd /home/pi/.jagex/runescape/bin || exit
 wget https://github.com/MrHDR/RaspberryPi/raw/master/Rsinstall/jagexappletviewer.jar
-cd /home/pi/.jagex/runescape/images
+cd /home/pi/.jagex/runescape/images || exit
 wget https://raw.githubusercontent.com/MrHDR/RaspberryPi/master/Rsinstall/jagexappletviewer.png
 wget https://raw.githubusercontent.com/MrHDR/RaspberryPi/master/Rsinstall/jagexappletvieweros.png
-cd /home/pi/Desktop
+cd /home/pi/Desktop || exit
 wget https://github.com/MrHDR/RaspberryPi/raw/master/Rsinstall/Rs3.desktop
 wget https://github.com/MrHDR/RaspberryPi/raw/master/Rsinstall/OSRS.desktop
+
+dialog --title "Runescape Installed" --msgbox "\nYou can launch the game from the destkop\n" 10 30
+exit
+
