069241b8f0dbfe60885df334058df6ff5d35ec9a
Martin Refseth <20230450+HDR@users.noreply.github.com>
2020-05-30 01:05:39 +0200

    Update for pi4 compatability

--- Rsinstall/RsInstall.sh
+++ Rsinstall/RsInstall.sh
@@ -2,7 +2,7 @@
 sudo apt-get install dialog
 sudo apt-get update | dialog --title "Updating Package database" --infobox "\nPlease wait\n" 5 50
 
-sudo apt-get -y install oracle-java8-jdk | dialog --title "Installing Java 8 JDK" --infobox "\nThis might take a while, please wait\n" 5 50
+sudo apt-get -y install default-jdk | dialog --title "Installing Java JDK" --infobox "\nThis might take a while, please wait\n" 5 50
 
 dialog --title "Creating Folders" --infobox "\nPlease wait\n" 5 50
 
@@ -15,13 +15,13 @@ mkdir /home/pi/.jagex/runescape/images
 
 dialog --title "Downloading & Installing Client" --infobox "\nPlease wait\n" 5 50
 cd /home/pi/.jagex/runescape/bin || exit
-wget https://github.com/MrHDR/RaspberryPi/raw/master/Rsinstall/jagexappletviewer.jar
+wget https://github.com/HDR/RaspberryPi/raw/master/Rsinstall/jagexappletviewer.jar
 cd /home/pi/.jagex/runescape/images || exit
-wget https://raw.githubusercontent.com/MrHDR/RaspberryPi/master/Rsinstall/jagexappletviewer.png
-wget https://raw.githubusercontent.com/MrHDR/RaspberryPi/master/Rsinstall/jagexappletvieweros.png
+wget https://raw.githubusercontent.com/HDR/RaspberryPi/master/Rsinstall/jagexappletviewer.png
+wget https://raw.githubusercontent.com/HDR/RaspberryPi/master/Rsinstall/jagexappletvieweros.png
 cd /home/pi/Desktop || exit
-wget https://github.com/MrHDR/RaspberryPi/raw/master/Rsinstall/Rs3.desktop
-wget https://github.com/MrHDR/RaspberryPi/raw/master/Rsinstall/OSRS.desktop
+wget https://github.com/HDR/RaspberryPi/raw/master/Rsinstall/Rs3.desktop
+wget https://github.com/HDR/RaspberryPi/raw/master/Rsinstall/OSRS.desktop
 
 dialog --title "Runescape Installed" --msgbox "\nYou can launch the game from the destkop\n" 10 30
 exit
