5e58c9411470be9b1c8a56c02aa0034d29c5da24
TheDublord <moltenmaniac@att.net>
2021-04-26 13:22:37 -0700

    Snip snip
    Cut the fat from the script. Thought I'd help. Luv U boo
    
    this is my 3rd time trying to PR why is this so difficult if this 404s im giving up

--- Rsinstall/RsInstall.sh
+++ Rsinstall/RsInstall.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
-sudo apt-get install dialog
-sudo apt-get update | dialog --title "Updating Package database" --infobox "\nPlease wait\n" 5 50
-
-sudo apt-get -y install default-jdk | dialog --title "Installing Java JDK" --infobox "\nThis might take a while, please wait\n" 5 50
-
-dialog --title "Creating Folders" --infobox "\nPlease wait\n" 5 50
-
+sudo apt-get update
+sudo apt-get install default-jdk -y
 mkdir /home/pi/.jagex_cache_32
 mkdir /home/pi/.jagex_cache_32/runescape
 mkdir /home/pi/.jagex
 mkdir /home/pi/.jagex/runescape
 mkdir /home/pi/.jagex/runescape/bin
 mkdir /home/pi/.jagex/runescape/images
-
-dialog --title "Downloading & Installing Client" --infobox "\nPlease wait\n" 5 50
-cd /home/pi/.jagex/runescape/bin || exit
-wget https://github.com/HDR/RaspberryPi/raw/master/Rsinstall/jagexappletviewer.jar
-cd /home/pi/.jagex/runescape/images || exit
-wget https://raw.githubusercontent.com/HDR/RaspberryPi/master/Rsinstall/jagexappletviewer.png
-wget https://raw.githubusercontent.com/HDR/RaspberryPi/master/Rsinstall/jagexappletvieweros.png
-cd /home/pi/Desktop || exit
-wget https://github.com/HDR/RaspberryPi/raw/master/Rsinstall/Rs3.desktop
-wget https://github.com/HDR/RaspberryPi/raw/master/Rsinstall/OSRS.desktop
-
-dialog --title "Runescape Installed" --msgbox "\nYou can launch the game from the destkop\n" 10 30
+wget -P /home/pi/.jagex/runescape/bin https://github.com/HDR/RaspberryPi/raw/master/Rsinstall/jagexappletviewer.jar
+wget -P cd /home/pi/.jagex/runescape/image https://raw.githubusercontent.com/HDR/RaspberryPi/master/Rsinstall/jagexappletviewer.png
+wget -P cd /home/pi/.jagex/runescape/image https://raw.githubusercontent.com/HDR/RaspberryPi/master/Rsinstall/jagexappletvieweros.png
+wget -P /home/pi/Desktop https://github.com/HDR/RaspberryPi/raw/master/Rsinstall/Rs3.desktop
+wget -P /home/pi/Desktop https://github.com/HDR/RaspberryPi/raw/master/Rsinstall/OSRS.desktop
 exit
+
 
